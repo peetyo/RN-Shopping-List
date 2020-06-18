bbaf669178073965ef9756da629f98b3198f51fc
Petar <petar.bozhidarov.todorov@gmail.com>
2020-06-18 11:29:50 +0200

    Delete item func added

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import  Header from './components/Header'
+import ListItem from './components/ListItem'
 // import { uuid } from 'uuidv4'
 import uuid from 'uuid-random'
 
 const App = () => {
   const [items, setItems] = useState([
     {id: uuid(), text: 'Vanilla Milk'},
-    {id: uuid(), text: 'Vanilla Milk'},
-    {id: uuid(), text: 'Vanilla Milk'},
+    {id: uuid(), text: 'Oats'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Hummus'},
   ]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  }
 
   return (
     <View style={styles.container}>
       <Header title="Shopping List"/>
+      <FlatList 
+        data={items} 
+        renderItem={({item})=>(
+        <ListItem item={item} deleteItem={deleteItem}/>
+      )}/>
     </View>
   )
 }
